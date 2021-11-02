327165b2cc871b34da840223a3618951a92145c1
andrehideki <andre.hkiw@gmail.com>
2021-11-02 13:48:56 -0300

    add: Db

---
+++ src/Db.js
@@ -0,0 +1,11 @@
+const Db = [
+  {
+    id: 1,
+    name: 'Manga',
+    date: new Date(),
+    description: '... some random description',
+    value: -100
+  }
+]
+
+export { Db }
--- src/Renderer.js
+++ src/Renderer.js
@@ -2,12 +2,20 @@ const Renderer = {
   
   renderEntries(entries) {
     const tableEntries = document.getElementById('table_entries');
-    tableEntries.innerHTML =
-      entries = entries.map(entry => (`
-        <tr>
-          <td>${entry}</td>
-        </tr>`
+    const tableHeaders = ['Data', 'Nome', 'Descrição', 'Valor']
+      .map(header => `<th>${header}</th>`).join('')
+    const tableRows = entries.map(entry => (`
+      <tr>
+        <td>${entry}</td>
+      </tr>`
       ))
+      .join('')
+    tableEntries.innerHTML = `
+      <thead>
+        <tr>${tableHeaders}</tr>
+      </thead>
+      <tbody>${tableRows}</tbody>
+    `  
   }
 }
 
