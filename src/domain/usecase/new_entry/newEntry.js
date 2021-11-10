4569b97f404e08e850915b665a6aa506ea6fcbfd
andrehideki <andre.hkiw@gmail.com>
2021-11-10 15:27:15 -0300

    config: jest and babel

--- src/domain/usecase/new_entry/newEntry.js
+++ src/domain/usecase/new_entry/newEntry.js
@@ -1,7 +1,9 @@
 // const Entry = require('../../model/Entry.js');
 import Entry from '../../model/Entry';
 
-export default newEntry = ({ name, description, date, value }, entryRepository) => {
+const newEntry = ({ name, description, date, value }, entryRepository) => {
   const entry = new Entry({ name, description, date, value });
   entryRepository.persist(entry);
 }
+
+export default newEntry;
