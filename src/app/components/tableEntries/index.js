214fc05b9146be1d93a592793fbee119e7025084
andrehideki <andre.hkiw@gmail.com>
2021-11-11 17:24:07 -0300

    fix: putting , on tableEntries

--- src/app/components/tableEntries/index.js
+++ src/app/components/tableEntries/index.js
@@ -30,7 +30,7 @@ const tableEntries = {
               <td>${entry.date}</td>
               <td>${entry.value}</td>
             </tr>
-          `)} 
+          `).join('')} 
         `
       });
   }
