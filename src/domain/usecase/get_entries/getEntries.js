22d312f56a0c07b194e9410ef5ac34dcc4a93bf0
andrehideki <andre.hkiw@gmail.com>
2021-11-16 15:45:31 -0300

    fix: date

--- src/domain/usecase/get_entries/getEntries.js
+++ src/domain/usecase/get_entries/getEntries.js
@@ -2,16 +2,16 @@ import Period from '../../model/Period';
 
 const getEntries = async ({ initialDate, endDate }, { entryRepository }) => {
   const period = (!!initialDate && !!endDate)? new Period({ initialDate, endDate }) : undefined;
-  const entries = await entryRepository.find({ period: period });
-  entries.map(entry => {
-      return {
-        id: entry.id,
-        name: entry.name,
-        description: entry.description,
-        date: entry.date.toISOString().substring(0, 10),
-        value: entry.value
-      }
-    });
+  const entryMappers = await entryRepository.find({ period: period });
+  const entries = entryMappers.map(entry => {
+    return {
+      id: entry.id,
+      name: entry.name,
+      description: entry.description,
+      date: entry.date.toISOString().substring(0, 10),
+      value: entry.value
+    }
+  });
   return entries;
 }
 
