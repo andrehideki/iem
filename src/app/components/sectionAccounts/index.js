d3040f5ea4a2830a0a1c05af32198ffefd043e4f
andrehideki <andre.hkiw@gmail.com>
2021-11-21 11:56:01 -0300

    add: account event

--- src/app/components/sectionAccounts/index.js
+++ src/app/components/sectionAccounts/index.js
@@ -1,21 +1,26 @@
+import eventEmitter from "../../eventEmitter";
 import { getRequest } from "../../utils/ajax";
 import { formatToBRL } from "../../utils/money";
 
 const secionAccounts = {
-  init(target) {
-    getRequest('account', {})
-      .then(data => data.json())
-      .then(accounts => {
-        target.innerHTML = `
-          <div class="d-flex flex-row">
-            ${ accounts.map(account => (`
-              <div class="badge rounded-pill bg-light text-dark fs-6">
-                ${account.name}: ${ formatToBRL(account.balance || 0) }
-              </div>
-             `)).join('') }
+  async init(target) {
+    const triggerEvents = ['newEntry', 'deleteEntry', 'updateEntry', 'filterChange'];
+    eventEmitter.on(triggerEvents, async () => await secionAccounts.getAccounts(target))
+    await secionAccounts.getAccounts(target);
+  },
+
+  async getAccounts(target) {
+    const data = await getRequest('account', {});
+    const accounts = await data.json();
+    target.innerHTML = `
+      <div class="d-flex flex-row">
+        ${ accounts.map(account => (`
+          <div class="badge rounded-pill bg-light text-dark fs-6">
+            ${account.name}: ${ formatToBRL(account.balance || 0) }
           </div>
-        `;
-      });
+        `)).join('') }
+      </div>
+    `;
   }
 }
 
--- src/infra/repository/EntryRepositoryDatabase.js
+++ src/infra/repository/EntryRepositoryDatabase.js
@@ -61,7 +61,7 @@ export default class extends EntryRepository {
             }
           }, {
             account: {
-              [Op.like]: `%${account || ''}%`
+              [Op.like]: `${account || '%'}`
             }
           }
         ],
