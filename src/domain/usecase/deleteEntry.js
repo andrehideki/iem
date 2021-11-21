d154e21e6f20e2493a961be6cf7c765372c54a0a
andrehideki <andre.hkiw@gmail.com>
2021-11-21 11:37:20 -0300

    add: deleteEntry updating account

--- src/domain/usecase/deleteEntry.js
+++ src/domain/usecase/deleteEntry.js
@@ -1,4 +1,8 @@
-const deleteEntry = async (id, { entryRepository }) => {
+const deleteEntry = async (id, { entryRepository, accountRepository }) => {
+  const entry = await entryRepository.get(id);
+  const account = await accountRepository.get(entry.account);
+  account.subtract(entry.value);
+  await accountRepository.update(account);
   await entryRepository.delete(id);
 }
 
